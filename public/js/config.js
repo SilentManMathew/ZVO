6b414211d93ec78fa6d7e7e26e9b7eba3f2f7b45
SilentManMathew <92011765+SilentManMathew@users.noreply.github.com>
2023-12-13 09:47:41 +0100

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_Qh48UIZ1J', // e.g. eu-east-2_uXboG5pAb
-        userPoolClientId: '15hhvh7cnct9imuatln22p82hr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-west-1_zW56uICtm', // e.g. eu-east-2_uXboG5pAb
+        userPoolClientId: '6lvdroics5f397fc5lh781bqdr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
